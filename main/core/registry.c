7036ee1908f1905538ea452ae12d4b61062dffe5
pythcoiner <pythcoiner@proton.me>
2026-04-22 15:24:02 -0400

    descriptor-rework: registry: types, REGISTRY_MAX_ENTRIES, static storage

--- main/core/registry.c
+++ main/core/registry.c
@@ -1,1 +1,4 @@
 #include "registry.h"
+
+static registry_entry_t registry_entries[REGISTRY_MAX_ENTRIES];
+static size_t registry_len = 0;
--- main/core/registry.h
+++ main/core/registry.h
@@ -2,9 +2,24 @@
 #define REGISTRY_H
 
 #include <stddef.h>
+#include <stdint.h>
 
 #include <wally_descriptor.h>
 
+#include "ss_whitelist.h"
 #include "storage.h"
+
+#define REGISTRY_MAX_ENTRIES  16
+#define REGISTRY_ID_MAX_LEN   32
+
+typedef struct {
+  char id[REGISTRY_ID_MAX_LEN];
+  storage_location_t loc;
+  struct wally_descriptor *desc;
+  size_t my_key_index;
+  size_t num_paths;
+  uint32_t origin_path[MAX_KEYPATH_ORIGIN_DEPTH];
+  size_t origin_path_len;
+} registry_entry_t;
 
 #endif // REGISTRY_H
