e3d32b0f5c589ea84319b35e9462f38e2f1c915a
pythcoiner <pythcoiner@proton.me>
2026-04-22 14:30:44 -0400

    descriptor-rework: ss_scriptpubkey P2PKH branch

--- main/core/ss_whitelist.c
+++ main/core/ss_whitelist.c
@@ -1,7 +1,9 @@
 #include "ss_whitelist.h"
 #include "key.h"
 #include <stdio.h>
+#include <string.h>
 #include <wally_bip32.h>
+#include <wally_crypto.h>
 #include <wally_script.h>
 
 bool ss_keypath_parse(const unsigned char *keypath_after_fp,
@@ -97,7 +99,40 @@ bool ss_scriptpubkey(ss_script_type_t script, uint32_t account,
       bip32_key_free(derived_key);
       return ret == WALLY_OK;
     }
-    case SS_SCRIPT_P2PKH:
+    case SS_SCRIPT_P2PKH: {
+      ss_keypath_t kp = {
+        .script  = SS_SCRIPT_P2PKH,
+        .purpose = 44,
+        .coin    = is_testnet ? 1u : 0u,
+        .account = account,
+        .chain   = chain,
+        .index   = index,
+      };
+      char path[SS_KEYPATH_FMT_MAX];
+      if (!ss_keypath_format(&kp, path, sizeof(path)))
+        return false;
+
+      struct ext_key *derived_key = NULL;
+      if (!key_get_derived_key(path, &derived_key))
+        return false;
+
+      uint8_t pkh20[HASH160_LEN];
+      int ret = wally_hash160(
+          derived_key->pub_key, EC_PUBLIC_KEY_LEN,
+          pkh20, HASH160_LEN);
+      bip32_key_free(derived_key);
+      if (ret != WALLY_OK)
+        return false;
+
+      out[0]  = 0x76;               /* OP_DUP */
+      out[1]  = 0xa9;               /* OP_HASH160 */
+      out[2]  = 0x14;               /* push 20 bytes */
+      memcpy(out + 3, pkh20, 20);   /* 20-byte hash160 */
+      out[23] = 0x88;               /* OP_EQUALVERIFY */
+      out[24] = 0xac;               /* OP_CHECKSIG */
+      *out_len = 25;
+      return true;
+    }
     case SS_SCRIPT_P2SH_P2WPKH:
     case SS_SCRIPT_P2TR:
     default:
